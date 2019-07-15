4c3037a034e0e978a228ea8c691ac6db1032b638
dvkom <d.v.kombarov@gmail.com>
2019-07-15 22:45:50 +0700

    holder idiom implemented

--- 15/5/BrowsersStatistic/src/main/java/utils/ConnectionFactory.java
+++ 15/5/BrowsersStatistic/src/main/java/utils/ConnectionFactory.java
@@ -8,31 +8,35 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
   private static final Logger log = Logger.getLogger(ConnectionFactory.class);
-  private static volatile Connection connection;
   private static final String DB_NAME = "learn";
   private static final String DB_USER = "root";
   private static final String DB_PASS = "mmm333";
 
   public static Connection getConnection() {
-    if (connection == null) {
+    return ConnectionHolder.instance;
+  }
+
+  private static class ConnectionHolder {
+    private static volatile Connection instance;
+
+    static {
       try {
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DB_NAME +
-            "?useSSL=false" + "&user=" + DB_USER + "&password=" + DB_PASS +
-            "&rewriteBatchedStatements=true");
+        instance = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DB_NAME +
+                "?useSSL=false" + "&user=" + DB_USER + "&password=" + DB_PASS +
+                "&rewriteBatchedStatements=true");
         createTable();
       } catch (SQLException e) {
         log.error(e);
       }
     }
-    return connection;
-  }
 
-  private static void createTable() throws SQLException {
-    connection.createStatement().execute("CREATE TABLE " +
-        "IF NOT EXISTS site_visit(" +
-        "id INT NOT NULL AUTO_INCREMENT, " +
-        "browser VARCHAR(50), " +
-        "visitDate DATETIME, " +
-        "PRIMARY KEY(id))");
+    private static void createTable() throws SQLException {
+      instance.createStatement().execute("CREATE TABLE " +
+          "IF NOT EXISTS site_visit(" +
+          "id INT NOT NULL AUTO_INCREMENT, " +
+          "browser VARCHAR(50), " +
+          "visitDate DATETIME, " +
+          "PRIMARY KEY(id))");
+    }
   }
 }
